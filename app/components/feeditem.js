83fe4bcd6d00113f6fe797e76cee12848ed17f19
Jonathan Shepherd <jshepherd@umass.edu>
2017-04-07 15:33:26 -0400

    fb6

--- app/components/feeditem.js
+++ app/components/feeditem.js
@@ -4,7 +4,26 @@ import CommentThread from './commentthread';
 import Comment from './comment';
 
 export default class FeedItem extends React.Component {
+  constructor(props) {
+    super(props);
+    // The FeedItem's initial state is what the Feed passed to us.
+    this.state = props.data;
+  }
+
+  handleCommentPost(commentText) {
+    // Post a comment as user ID 4, which is our mock user!
+    postComment(this.state._id, 4, commentText, (updatedFeedItem) => {
+      // Update our state to trigger a re-render.
+      this.setState(updatedFeedItem);
+    });
+  }
+
   render() {
+    // Render using data from state.
+    var data = this.state;
+    // The rest of this method is the same as before!
+    // Except you need to change the CommentThread element to hook up an onPost function:
+    // <CommentThread onPost={(commentText) => this.handleCommentPost(commentText)}>
   var data = this.props.data;
   var contents;
   switch(data.type) {
@@ -14,7 +33,12 @@ export default class FeedItem extends React.Component {
       // same key as the FeedItem.
       contents = (
         <StatusUpdate key={data._id} author={data.contents.author} postDate={data.contents.postDate} location={data.contents.location}>
-          {data.contents.contents}
+          {data.contents.contents.split("\n").map((line, i) => {
+  // Note: 'i' is the index of line in data.contents.contents.
+  return (
+    <p key={"line" + i}>{line}</p>
+  );
+})}}
         </StatusUpdate>
       );
       break;
@@ -59,10 +83,68 @@ data.comments.map((comment, i) => {
           </div>
         </div>
         <hr />
-        <CommentThread>
+        <CommentThread onPost={(commentText) => this.handleCommentPost(commentText)}>
           { comments }
         </CommentThread>
       </div>
     </div>
   )
 }
+ /**
+   * Triggered when the user clicks on the 'like' or 'unlike' button.
+   */
+  handleLikeClick(clickEvent) {
+    // Stop the event from propagating up the DOM tree, since we handle it here.
+    // Also prevents the link click from causing the page to scroll to the top.
+    clickEvent.preventDefault();
+    // 0 represents the 'main mouse button' -- typically a left click
+    // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+    if (clickEvent.button === 0) {
+      // Callback function for both the like and unlike cases.
+      var callbackFunction = (updatedLikeCounter) => {
+        // setState will overwrite the 'likeCounter' field on the current
+        // state, and will keep the other fields in-tact.
+        // This is called a shallow merge:
+        // https://facebook.github.io/react/docs/component-api.html#setstate
+        this.setState({likeCounter: updatedLikeCounter});
+      };
+
+      if (this.didUserLike()) {
+        // User clicked 'unlike' button.
+        unlikeFeedItem(this.state._id, 4, callbackFunction);
+      } else {
+        // User clicked 'like' button.
+        likeFeedItem(this.state._id, 4, callbackFunction);
+      }
+    }
+  }
+
+  /**
+   * Returns 'true' if the user liked the item.
+   * Returns 'false' if the user has not liked the item.
+   */
+  didUserLike() {
+    var likeCounter = this.state.likeCounter;
+    var liked = false;
+    // Look for a likeCounter entry with userId 4 -- which is the
+    // current user.
+    for (var i = 0; i < likeCounter.length; i++) {
+      if (likeCounter[i]._id === 4) {
+        liked = true;
+        break;
+      }
+    }
+    return liked;
+  }
+
+  render() {
+    var likeButtonText = "Like";
+    if (this.didUserLike()) {
+      likeButtonText = "Unlike";
+    }
+    // Skipping the first part of this method, which is unchanged.
+                <a href="#" onClick={(e) => this.handleLikeClick(e)}>
+                  <span className="glyphicon glyphicon-thumbs-up"></span> {likeButtonText}
+                </a>
+    // The rest of this method is unchanged
+  }
